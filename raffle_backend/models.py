61db3417e4170608187fffb5f859a384e3b170a4
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2020-07-03 17:00:02 -0700

    Working on host test cases. Added method for create session

--- raffle_backend/models.py
+++ raffle_backend/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.crypto import get_random_string
 
 
 class User(models.Model):
@@ -20,10 +21,12 @@ class Winner(models.Model):
 class Session(models.Model):
     session_id = models.CharField(
         "Session_ID", primary_key=True, max_length=50)
-    participant = models.ForeignKey(User, on_delete=models.CASCADE)
+    participant = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
     started_time = models.DateTimeField("Start Time", auto_now=True)
-    end_time = models.DateField("End Time")
-    winner = models.ForeignKey(Winner, on_delete=models.CASCADE)
+    end_time = models.DateField("End Time", blank=True, null=True)
+    winner = models.ForeignKey(
+        Winner, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.session_id
@@ -40,13 +43,38 @@ class Session(models.Model):
     def add_participants(self):
         pass
 
+
 class Host(models.Model):
     name = models.CharField("Name", max_length=50)
-    host_token = models.CharField("Host Token", primary_key=True, max_length=50)
-    session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
+    host_token = models.CharField(
+        "Host Token", primary_key=True, max_length=50)
+    session = models.ForeignKey(
+        Session, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
-    
+
     def get_session(self):
-        pass
+        return None
+
+    def create_session(self, input_session_id: str = None):
+        if self.session is None:
+            new_session_id = input_session_id
+            if input_session_id is None:
+                new_session_id = self._get_unused_session_token()
+            tmp_session = Session.objects.create(session_id=new_session_id)
+            self.session_id = tmp_session
+
+    def _get_unused_session_token(self):
+        NUM_CHARS = 6
+        token = None
+        token_already_exists = True
+        # Loop until we find a new token
+        while token_already_exists:
+            token = get_random_string(length=NUM_CHARS)
+            try:
+                Session.objects.get(session_id=token)
+            except Session.DoesNotExist:
+                token_already_exists = False
+
+        return token
--- raffle_backend/tests/test_models.py
+++ raffle_backend/tests/test_models.py
@@ -6,6 +6,43 @@ from raffle_backend.models import (
     Host
 )
 
+
 class TestUser(TestCase):
     def test_base_constructor(self):
-        pass
+        user = User(name='Fred', ip_address='127.0.0.1')
+        self.assertTrue(isinstance(user, User))
+        self.assertEqual('Fred', str(user))
+
+
+class TestHost(TestCase):
+
+    def test_constructor_without_session_id(self):
+        host = Host(name='Velma', host_token='AkSefj90j#89wj4$GH9s4S)$Fj')
+        self.assertTrue(isinstance(host, Host))
+        self.assertEqual('Velma', str(host))
+
+    def test_create_session_does_not_exist_yet(self):
+        host = Host(name='Velma', host_token='AkSefj90j#89wj4$GH9s4S)$Fj')
+        expected_id = 'ABC123'
+
+        self.assertIsNone(host.session_id)
+        host.create_session(expected_id)
+        self.assertEqual(expected_id, host.session.session_id)
+
+    def test_create_session_already_exists(self):
+        host = Host(name='Velma', host_token='AkSefj90j#89wj4$GH9s4S)$Fj')
+        expected_id = 'ABC123'
+
+        self.assertIsNone(host.session_id)
+        host.create_session(expected_id)
+        # Create another one
+        host.create_session('SOME_OTHER_SESSION_ID')
+        self.assertEqual(expected_id, host.session.session_id)
+
+
+class TestSession(TestCase):
+    pass
+
+
+class TestWinner(TestCase):
+    pass

4d4063bf5b721d88f726b3780b6ed46d79dcb5a8
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2020-08-16 21:12:19 -0700

    Working on form for create session

--- raffle_frontend/src/pages/PageCreateSession.js
+++ raffle_frontend/src/pages/PageCreateSession.js
@@ -1,8 +1,36 @@
 import React from "react";
 
 class PageCreateSession extends React.Component {
+  state = {
+    sessionName: null,
+    hostName: null,
+  }
   render() {
-    return <div>PageCreateSession</div>;
+    return (
+      <div className="ui container">
+        <h1 className="ui header centered">Host a Session</h1>
+        <div className="ui form">
+          <div className="item">
+            <label>Host Name</label>
+            <input
+              type="text"
+              onChange={(event) =>
+                this.setState({ hostName: event.target.value })
+              }
+            />
+          </div>
+          <div className="item">
+            <label>Session Name</label>
+            <input
+              type="text"
+              onChange={(event) =>
+                this.setState({ sessionName: event.target.value })
+              }
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 

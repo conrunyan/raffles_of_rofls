a7e2859331e8c71fe81ceef995025fb22c27182b
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2020-08-20 19:55:01 -0700

    Added redux and redux form

--- raffle_frontend/src/App.js
+++ raffle_frontend/src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
 
 import PageHome from "./pages/PageHome";
 import PageCreateSession from "./pages/PageCreateSession";
 
+import reducers from "./redux/reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
+
 const App = () => {
   return (
-    <div className="ui container">
-      <HashRouter>
-        <Route path="/" exact component={PageHome} />
-        <Route path="/host/create" exact component={PageCreateSession} />
-      </HashRouter>
-    </div>
+    <Provider store={store}>
+      <div className="ui container">
+        <HashRouter>
+          <Route path="/" exact component={PageHome} />
+          <Route path="/host/create" exact component={PageCreateSession} />
+        </HashRouter>
+      </div>
+    </Provider>
   );
 };
 

a9237ed397916914f7abd4ae53b4890c7dda925a
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2020-08-20 20:08:51 -0700

    Hooked up redux form to create session component

--- raffle_frontend/src/pages/PageCreateSession.js
+++ raffle_frontend/src/pages/PageCreateSession.js
@@ -1,38 +1,66 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
 
 class PageCreateSession extends React.Component {
-  state = {
-    sessionName: null,
-    hostName: null,
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+  renderInput = ({ input, label, meta }) => {
+    console.log(meta);
+    const className = meta.touched && meta.error ? "field error" : "field";
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    );
   };
+
   render() {
     return (
       <div className="ui container">
         <h1 className="ui header centered">Host a Session</h1>
-        <div className="ui form">
-          <div className="item">
-            <label>Host Name</label>
-            <input
-              type="text"
-              onChange={(event) =>
-                this.setState({ hostName: event.target.value })
-              }
-            />
-          </div>
-          <div className="item">
-            <label>Session Name</label>
-            <input
-              type="text"
-              onChange={(event) =>
-                this.setState({ sessionName: event.target.value })
-              }
-            />
-          </div>
+        <form className="ui form error">
+          <Field
+            name="hostName"
+            component={this.renderInput}
+            label={"Host Name"}
+          />
+          <Field
+            name="sessionName"
+            component={this.renderInput}
+            label={"Session Name"}
+          />
           <div className="ui submit button">Create Session</div>
-        </div>
+        </form>
       </div>
     );
   }
 }
 
-export default PageCreateSession;
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.hostName) {
+    errors.hostName = "Host Name cannot be blank.";
+  }
+  if (!formValues.sessionName) {
+    errors.sessionName = "Session Name cannot be blank.";
+  }
+
+  return errors;
+};
+
+const formWrapped = reduxForm({
+  form: "streamCreate",
+  validate: validate,
+})(PageCreateSession);
+
+export default connect(null, {})(formWrapped);

0d601c8198a33e8b331effbfe9a6af62cf604de5
Connor Runyan <connor.runyan@aggiemail.usu.edu>
2020-09-17 21:17:16 -0700

    Deleted old folder structure

--- raffle_frontend/src/pages/PageSessionLobbyHost.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class PageSessionLobbyHost extends React.Component {
-    render() {
-        return <div>PageSessionLobbyHost</div>
-    }
-}
-
-export default PageSessionLobbyHost;
